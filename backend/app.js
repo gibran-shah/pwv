edfcdf1d480c83d148583e96d621121ce5a3653e
gibran-shah <gshah@assertivesolutions.ca>
2023-01-24 21:00:06 -0700

    Added ajax; added backend

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const exp = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const importRoutes = require('./routes/import');
+const addRoutes = require('./routes/add');
+const fetchRoutes = require('./routes/fetch');
+
+dotenv.config();
+
+const app = exp();
+
+const port = process.env.PORT;
+
+const corsOptions = {
+    origin: [
+		'http://localhost:3000',
+		'http://www.shahspace.com',
+		'https://www.shahspace.com',
+		'http://shahspace.com',
+		'https://shahspace.com',
+	],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+fs.appendFileSync('log.txt', new Date().toString() + ': CORS set up.\n');
+
+app.use('/import', importRoutes);
+app.use('/add', addRoutes);
+app.use('/fetch', fetchRoutes);
+
+app.listen(port);
+
+fs.appendFileSync('log.txt', new Date().toString() + ': app.js is running and listening on port ' + port + '\n');
+
+console.log('Listening on port', port);
+
+module.exports = app;
+
---
+++ logic.js
@@ -0,0 +1,22 @@
+function pageLoad() {
+    console.log('hello');
+}
+
+function importFile() {
+    console.log('import file');
+    const xhttp = new XMLHttpRequest();
+    const url = 'http://localhost:3000';
+
+    xhttp.onreadystatechange = function() {
+      console.log('readyState: ', this.readyState);
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+            console.log('success!');
+        } else {
+            console.log('error: ', JSON.stringify(this));
+        }
+      }  
+    };
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
