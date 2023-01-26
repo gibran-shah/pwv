517c988648d6ad7dfafc3b9403061fbeff593132
gibran-shah <gshah@assertivesolutions.ca>
2023-01-25 21:04:04 -0700

    Reading actual passwords file

--- backend/routes/import.js
+++ backend/routes/import.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
 const exp = require('express');
 
 const router = exp.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('import get');
-    res.status(200).send('Import successful');
+    fs.readFile(process.env.IMPORT_FILE, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`err: ${JSON.stringify(err)}`);
+            res.status(500).send('Can\'t read file');
+            return;
+        }
+
+        const dataArray = data.split('\r\n');
+        for (let i = 0; i < dataArray.length; i++) {
+            console.log(`dataArray[${i}]: ${JSON.stringify(dataArray[i])}`);
+        }
+
+        res.status(200).send('Import successful');
+    });
 });
 
 module.exports = router;
