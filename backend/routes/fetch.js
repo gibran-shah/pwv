2a58d9fcff7e98921338b49c1d86c59c2456557d
gibran-shah <gshah@assertivesolutions.ca>
2023-02-28 21:17:40 -0700

    sorting records, grouping matching records, and encrypting rows during import

--- backend/routes/fetch.js
+++ backend/routes/fetch.js
@@ -16,9 +16,10 @@ router.get('/', (req, res, next) => {
             return;
         }
         const allRecords = getAllRecords().then(allRecords => {
-            const decryptedRecords = decryptRecords(allRecords);
-            const matchingRecords = findMatchingRecords(decryptedRecords, searchString);
-            res.status(200).send(matchingRecords);
+            const allRecordsSorted = sortRecords(allRecords);
+            const decryptedRecords = decryptRecords(allRecordsSorted);
+            const matchingRecordGroups = findMatchingRecords(decryptedRecords, searchString, 1, 5);
+            res.status(200).send(matchingRecordGroups);
         });        
     } else {
         res.status(403).send('Unauthorized');
@@ -35,6 +36,8 @@ const getAllRecords = () => {
         .then(snapshot => snapshot.docs.map(d => d.data()));
 };
 
+const sortRecords = (records) => records.sort((r1, r2) => r1.line > r2.line ? 1 : -1);
+
 const decryptRecords = (records) => {
     const decryptedRecords = [];
     const privateKey = 'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIVxwpKlP/DFPJZfMpQheCnwRm6H5PM0HXbNNkdb73CwT06X7+TT6xyBbqpmPVkdBzqn8sB3Z9mlKWFHrErl8iOdQ8QVuPPJRePwGmWYuavHk/qBJwypWIXwYgyAIljS55Hwv/PQpyKJLZU8uQXSCzWk3AsYY008OTxSmVpxRBENAgMBAAECgYAPhhRkiuTNVaSeuaRgpyQbfOA0q4pu4HktAOJ3mrJYIaqYMgSuUhjf0A7Xs90fKMlWgYMz8LAcGaKetn/MlsN8RfohX9UIHl1QbOiV7tD8z/I8Jzv0ycgwsDjOVwxv6f9/0nlDH30DWHPFF/PVcu2nCscCfwsCVIxYFttPPFlLAQJBAMopgQtA2A6qpEtNz+42C5Y15HcCRkqhU9m1DvVwDaz7d5HBqXzf9E8qu4Nf2eyKe9WdT/+3t2mUVVJ082EzbzUCQQCo+2RI2AJdNgR4tey4hNjXaPZK4PI7W8ey9y3mNFfTeVa+2y5dcamcW5RLLkdWfW+njcxakTa+irCheNCk3Z15AkEAhBGFKhLckwhLeJ6G3joUa7cdX9nujcbC/pDYCuN/K6jEPkZkMR4lfqqOAY9W0L7Pk4fHwWkFu0WaT9nzPWbamQJAXPz50pwLsP9otoRmH6P7L46y+cGPitqWqgUbXPKw4c6DzcMPYJyLmV9L25M77nxYVPvS2IPQfEoVqMWzCwZpAQJAPAP6u1oT7H+ZFO6DFUJlirNEc3+JhULCu97+JntaCnTpYFyRjQm9KFxdeTfVpTvLSgF2rz67YExqKS78r+jkZA==';
@@ -46,15 +49,44 @@ const decryptRecords = (records) => {
     return decryptedRecords;
 };
 
-const findMatchingRecords = (records, searchString) => {
-    const matchingRecords = [];
-    records.forEach(r => {
+const findMatchingRecords = (records, searchString, frontRange = 0, backRange = 0) => {
+    const matchingRecordGroups = [];
+    for (let i = 0; i < records.length; i++) {
+        const r = records[i];
         if (r.content.toLowerCase().includes(searchString.toLowerCase())) {
-            matchingRecords.push(r);
+            const recordGroup = [];
+            // Gather range of records just before matching record
+            if (frontRange) {
+                let fr_start = i - frontRange;
+                while (fr_start < 0) fr_start++;
+                for (let fr_i = fr_start; fr_i < i; fr_i++) {
+                    recordGroup.push(records[fr_i]);
+                } 
+            }
+            // Add matching record
+            recordGroup.push(r);
+            // Gather range of records just after matching record
+            if (backRange) {
+                let br_end = i + backRange;
+                while (br_end >= records.length) {
+                    br_end--;
+                }
+                let recordsSinceLastMatch = 0;
+                for (let br_i = i + 1; br_i <= br_end; br_i++) {
+                    recordsSinceLastMatch++;
+                    recordGroup.push(records[br_i]);
+                    if (records[br_i].content.toLowerCase().includes(searchString.toLowerCase())) {
+                        br_end += recordsSinceLastMatch;
+                        br_end = br_end >= records.length ? records.length - 1 : br_end;
+                        i = br_i;
+                        recordsSinceLastMatch = 0;
+                    }
+                } 
+            }
+            matchingRecordGroups.push(recordGroup);
         }
-    });
-    return matchingRecords;
+    }
+    return matchingRecordGroups;
 };
-
 
 module.exports = router;
--- backend/routes/import.js
+++ backend/routes/import.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const rsa = require('node-rsa');
 const exp = require('express');
 const fbAdmin = require('firebase-admin');
 const { utils } = require('./utils.js');
@@ -9,7 +10,8 @@ router.get('/', (req, res, next) => {
     try {
         utils.initFirebase();
         const fileRows = getFileRows();
-        storeFileRows(fileRows);
+        const encryptedFileRows = encryptFileRows(fileRows);
+        storeFileRows(encryptedFileRows);
         res.status(200);
     } catch (err) {
         console.log(`err = ${JSON.stringify(err)}`);
@@ -44,6 +46,16 @@ const storeFileRows = function(fileRows) {
     }
 
     batch.commit();
+}
+
+const encryptFileRows = function(fileRows) {
+    const encryptedRows = [];
+    fileRows.forEach((row) => {
+        const privateKey = 'MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIVxwpKlP/DFPJZfMpQheCnwRm6H5PM0HXbNNkdb73CwT06X7+TT6xyBbqpmPVkdBzqn8sB3Z9mlKWFHrErl8iOdQ8QVuPPJRePwGmWYuavHk/qBJwypWIXwYgyAIljS55Hwv/PQpyKJLZU8uQXSCzWk3AsYY008OTxSmVpxRBENAgMBAAECgYAPhhRkiuTNVaSeuaRgpyQbfOA0q4pu4HktAOJ3mrJYIaqYMgSuUhjf0A7Xs90fKMlWgYMz8LAcGaKetn/MlsN8RfohX9UIHl1QbOiV7tD8z/I8Jzv0ycgwsDjOVwxv6f9/0nlDH30DWHPFF/PVcu2nCscCfwsCVIxYFttPPFlLAQJBAMopgQtA2A6qpEtNz+42C5Y15HcCRkqhU9m1DvVwDaz7d5HBqXzf9E8qu4Nf2eyKe9WdT/+3t2mUVVJ082EzbzUCQQCo+2RI2AJdNgR4tey4hNjXaPZK4PI7W8ey9y3mNFfTeVa+2y5dcamcW5RLLkdWfW+njcxakTa+irCheNCk3Z15AkEAhBGFKhLckwhLeJ6G3joUa7cdX9nujcbC/pDYCuN/K6jEPkZkMR4lfqqOAY9W0L7Pk4fHwWkFu0WaT9nzPWbamQJAXPz50pwLsP9otoRmH6P7L46y+cGPitqWqgUbXPKw4c6DzcMPYJyLmV9L25M77nxYVPvS2IPQfEoVqMWzCwZpAQJAPAP6u1oT7H+ZFO6DFUJlirNEc3+JhULCu97+JntaCnTpYFyRjQm9KFxdeTfVpTvLSgF2rz67YExqKS78r+jkZA==';
+        const key = new rsa(`${privateKey}`, 'pkcs8');
+        encryptedRows.push(key.encrypt(row, 'base64'));
+    });
+    return encryptedRows;
 }
 
 module.exports = router;
