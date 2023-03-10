dbbc5c9fbaa16916146544237f33f3a8e5dbcd45
gibran-shah <gshah@assertivesolutions.ca>
2023-03-09 21:37:00 -0700

    using access token and expiration time

--- logic.js
+++ logic.js
@@ -1,3 +1,32 @@
+const { access } = require("fs");
+
+function pageLoad() {
+  if (isSignedIn()) {
+    renderSignedInContainer();
+  } else {
+    renderSignedOutContainer();
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    username.value = 'gibran.shah.pwv@gmail.com';
+    password.value = 'pwv.4real.123.@';
+  }
+}
+
+function isSignedIn() {
+  const accessToken = localStorage.getItem('accessToken');
+  console.log(`accessToken = ${accessToken}`);
+  if (accessToken) {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      console.log(`expirationTime = ${expirationTime}`);
+      return parseInt(expirationTime) > Date.now();
+    }
+    return false;
+  }
+
+  return false;
+}
+
 function importFile() {
     ajax('import', 'GET');
 }
@@ -24,13 +53,16 @@ function signIn() {
     'auth/signin',
     'POST',
     {username: username.value, password: password.value},
-    () => {
-      const signinContainer = document.getElementById('signin-container');
-      const signoutContainer = document.getElementById('signout-container');
-      signinContainer.style.display = 'none';
-      signoutContainer.style.display = 'block';
-      username.value = '';
-      password.value = '';
+    (response) => {
+      const responseObj = JSON.parse(response);
+      const { accessToken, expirationTime } = responseObj;
+      if (accessToken && expirationTime) {
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('expirationTime', expirationTime);
+        renderSignedInContainer();
+      } else {
+        console.log('Could not log in');
+      }
     }
   );
 }
@@ -40,13 +72,28 @@ function signOut() {
     'auth/signout',
     'POST',
     null,
-    () => {
-      const signinContainer = document.getElementById('signin-container');
-      const signoutContainer = document.getElementById('signout-container');
-      signinContainer.style.display = 'block';
-      signoutContainer.style.display = 'none';
+    (response) => {
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('expirationTime');
+      renderSignedOutContainer();
     }
   );
+}
+
+function renderSignedInContainer() {
+  const signinContainer = document.getElementById('signin-container');
+  const signoutContainer = document.getElementById('signout-container');
+  signinContainer.style.display = 'none';
+  signoutContainer.style.display = 'block';
+  username.value = '';
+  password.value = '';
+}
+
+function renderSignedOutContainer() {
+  const signinContainer = document.getElementById('signin-container');
+  const signoutContainer = document.getElementById('signout-container');
+  signinContainer.style.display = 'block';
+  signoutContainer.style.display = 'none';
 }
 
 function encodeCommas(array) {
@@ -68,8 +115,8 @@ function ajax(endpoint, method, payload, callback) {
     if (this.readyState === 4) {
       if (this.status === 200) {
           console.log('success!');
-          if (callback) {
-            callback();
+          if (callback instanceof Function) {
+            callback(this.response);
           }
       } else {
           console.log('error: ', JSON.stringify(this));
