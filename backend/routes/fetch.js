878b4ddfbe33d7d6910b2adf106bdeadb2053077
gibran-shah <gshah@assertivesolutions.ca>
2023-04-12 21:37:00 -0600

    refactored fetch for adding indexing

--- backend/routes/fetch.js
+++ backend/routes/fetch.js
@@ -11,73 +11,88 @@ router.get('/', (req, res, next) => {
     if (auth.currentUser) {
         const searchString = req.query.searchString.trim();
         if (!searchString || !searchString.length) {
-            res.status(200).send('Search string empty');
+            res.status(400).send('Search string empty');
             return;
         }
-        const allRecords = getAllRecords().then(allRecords => {
-            const allRecordsSorted = sortRecords(allRecords);
-            const decryptedRecords = decryptRecords(allRecordsSorted);
-            const matchingRecordGroups = findMatchingRecords(decryptedRecords, searchString, 1, 5);
-            res.status(200).send(matchingRecordGroups);
-        }, (err) => {
-            console.log(`err = ${JSON.stringify(err)}`);
-            res.status(500).send('Something went wrong when attempting to fetch all records');
-          //  res.status(200).send([]);
-            /*
-            res.status(200).send([
-                [
-                    {
-                        line: 15,
-                        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-                    },
-                    {
-                        line: 16,
-                        content: 'Lorem Ipsum has been the industry\'s standard aws dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-                    },
-                    {
-                        line: 17,
-                        content: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-                    }
-                ],
-                [
-                    {
-                        line: 500,
-                        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-                    },
-                    {
-                        line: 501,
-                        content: 'Lorem Ipsum has AWS been the industry\'s standard dummy text ever since the 1500s, when aws an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-                    },
-                    {
-                        line: 502,
-                        content: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-                    },
-                    {
-                        line: 503,
-                        content: 'Lorem Ipsum is simply dummy text of the aws printing and typesetting industry.'
-                    },
-                    {
-                        line: 504,
-                        content: 'Lorem Ipsum has been the industry\'s standard aws dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
-                    },
-                    {
-                        line: 505,
-                        content: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-                    }
-                ],
-                [
-                    {
-                        line: 1021,
-                        content: 'Lorem Ipsum is simply dummy text of the aws printing and typesetting aws industry.'
-                    }
-                ]
-            ]);
-            */
-        });        
+
+        const index = false; //getIndex(searchString);
+        if (index) {
+            searchByIndex(index);
+        } else {
+            searchAllRecords(searchString).then((recordGroups) => {
+                return createIndexes(recordGroups, searchString);
+            }).then((recordGroups) => {
+                res.status(200).send(recordGroups);
+            }).catch ((err) => {
+                console.log(`err = ${JSON.stringify(err)}`);
+                res.status(500).send('Something went wrong when attempting to fetch all records');
+            });
+        }      
     } else {
         res.status(403).send('Unauthorized');
     }
 });
+
+const createIndexes = (recordGroups, searchString) => {
+// https://firebase.google.com/docs/firestore/query-data/queries#node.js
+    if (!fbAdmin.apps.length) {
+        utils.initFirebase();
+    }
+
+    const firestore = fbAdmin.apps[0].firestore();
+    const indexesRef = firestore.collection('indexes');
+
+    return indexesRef.doc().set({
+        searchString,
+        lineIds: getIdsToIndex(recordGroups, searchString)
+    }).then(() => recordGroups.map(rg => rg.map(r => ({
+        line: r.line,
+        content: r.content
+    }))));
+};
+
+const getIdsToIndex = (recordGroups, searchString) => {
+    const idsToIndex = [];
+    for (let i = 0; i < recordGroups.length; i++) {
+        const group = recordGroups[i];
+        for (let j = 0; j < group.length; j++) {
+            if (group[j].content.toLowerCase().includes(searchString.toLowerCase())) {
+                idsToIndex.push(group[j].line);
+            }
+        }
+    }
+
+    return idsToIndex;
+};
+
+const getIndex = (searchString) => {
+    if (!fbAdmin.apps.length) {
+        utils.initFirebase();
+    }
+
+    // const firestore = fbAdmin.apps[0].firestore();
+    // const indexesRef = firestore.collection('indexes');
+    // const q = query(indexesRef, where('searchString', '==', true));
+}
+
+const searchByIndex = (index) => {
+    if (!fbAdmin.apps.length) {
+        utils.initFirebase();
+    }
+
+    // const firestore = fbAdmin.apps[0].firestore();
+    // return firestore.collection('indexes').get()
+    //     .then(snapshot => snapshot.docs.map(d => d.data()));
+};
+
+const searchAllRecords = async (searchString) => {
+    return getAllRecords().then(allRecords => {
+        const allRecordsSorted = sortRecords(allRecords);
+        const decryptedRecords = decryptRecords(allRecordsSorted);
+        const matchingRecordGroups = findMatchingRecords(decryptedRecords, searchString, 1, 5);
+        return matchingRecordGroups;
+    });  
+}
 
 const getAllRecords = () => {
     if (!fbAdmin.apps.length) {
@@ -86,7 +101,7 @@ const getAllRecords = () => {
 
     const firestore = fbAdmin.apps[0].firestore();
     return firestore.collection('lines').get()
-        .then(snapshot => snapshot.docs.map(d => d.data()));
+        .then(snapshot => snapshot.docs.map(d => Object.assign({ id: d.id }, d.data())));
 };
 
 const sortRecords = (records) => records.sort((r1, r2) => r1.line > r2.line ? 1 : -1);
@@ -95,6 +110,7 @@ const decryptRecords = (records) => {
     const decryptedRecords = [];
     const key = utils.getRsaKey();
     records.forEach(r => decryptedRecords.push({
+        id: r.id,
         line: r.line,
         content: key.decrypt(r.content, 'utf8')
     }));
